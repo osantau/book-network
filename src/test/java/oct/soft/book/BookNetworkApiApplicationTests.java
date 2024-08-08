ec67012d8796d797d933ad4e75191f64ef6b0953
osantau <octavian.santau@plastor.ro>
2024-08-08 10:56:27 +0300

    Initial project structure and configuration.

---
+++ src/test/java/oct/soft/book/BookNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package oct.soft.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
