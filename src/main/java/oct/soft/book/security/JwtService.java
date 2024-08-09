80f378cbd1da0dae340a1ca0018f42a745651cb6
osantau <octavian.santau@plastor.ro>
2024-08-09 09:23:06 +0300

    Fix type error.

--- src/main/java/oct/soft/book/security/JwtService.java
+++ src/main/java/oct/soft/book/security/JwtService.java
@@ -18,7 +18,7 @@ import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
-	@Value("${application.security.jwt.expiration  }")
+	@Value("${application.security.jwt.expiration}")
 	private long jwtExpiration;
 	@Value("${application.security.jwt.secret-key}")
 	private String secretKey;
